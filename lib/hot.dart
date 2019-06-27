c84b76499afda49ff483ef398248c3858051eedd
huangjieru <365708064@qq.com>
2019-06-27 16:53:09 +0800

    测试新加入文件

---
+++ lib/hot.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HotPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Text('热热热'),
+    );
+  }
+}
